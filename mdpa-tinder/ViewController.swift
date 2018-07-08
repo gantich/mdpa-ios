cb732da2587aa7d4f5835a35b059ce9d5e0124a9
Guillermo Antich <guillermoantich@gmail.com>
2018-07-08 14:05:17 +0200

    Initial Commit

---
+++ mdpa-tinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mdpa-tinder
+//
+//  Created by Guillermo Antich Soler on 8/7/18.
+//  Copyright © 2018 Guillermo Antich Soler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
